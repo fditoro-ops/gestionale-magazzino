3b75cae3f55e4694980b31bf8f8e1e3139706ae0
fditoro-ops <f.ditoro@icloud.com>
2026-03-26 21:22:20 +0100

    Update webhooks.cic.router.ts

--- apps/api/src/routes/webhooks.cic.router.ts
+++ apps/api/src/routes/webhooks.cic.router.ts
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import express, { Router } from "express";
 import { processCicWebhook } from "../services/cicWebhook.service";
 
 const router = Router();
@@ -7,6 +7,6 @@ router.get("/", (_req, res) => res.status(200).send("OK"));
 router.head("/", (_req, res) => res.status(200).end());
 router.options("/", (_req, res) => res.status(200).end());
 
-router.post("/", processCicWebhook);
+router.post("/", express.raw({ type: "*/*" }), processCicWebhook);
 
 export default router;
